3bc48d45464c14d9190e1c4a28b9edfe313a8586
thdfw <th.defauw@gmail.com>
2024-09-30 19:08:49 -0700

    Minor updates

--- running.py
+++ running.py
@@ -4,6 +4,6 @@ from closed_loop import closed_loop_simulation
 
 time_now = pendulum.datetime(2022, 12, 16, 21, 0, 0, tz='America/New_York')
 state_now = Node(time_slice=0, top_temp=50, thermocline=600)
-simulation_hours = 72
+simulation_hours = 48
 
 closed_loop_simulation(time_now, state_now, simulation_hours, plot_iteration=False)
