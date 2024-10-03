5b1bab370fc3ff4f2bb01fb41890b38f061c29ee
thdfw <th.defauw@gmail.com>
2024-10-02 20:46:40 -0700

    Renaming constants

--- dijkstra closedloop.py
+++ dijkstra closedloop.py
@@ -6,14 +6,14 @@ from matplotlib.colors import Normalize
 from past_data import get_data
 from cop import COP, to_celcius
 
-HORIZON = 48 # hours
+HORIZON_HOURS = 48
 NUM_LAYERS = 2400
-MIN_TOP_TEMP = 120 # F
-MAX_TOP_TEMP = 180 # F
-TEMP_LIFT = 20 # F
-TEMP_DROP = 20 # F
-HP_POWER = 12 # kW
-MASS_TANKS = 454.25*3 # kg
+MIN_TOP_TEMP_F = 120
+MAX_TOP_TEMP_F = 180
+TEMP_LIFT_F = 20
+TEMP_DROP_F = 20
+HP_POWER_KW = 12
+MASS_TANKS_KG = 454.25*3
 SPECIFIC_HEAT_WATER = 4.187/3600 # kWh/kg/K
 
 OVERESTIME_LOAD = 0 # %
@@ -28,16 +28,16 @@ class Node():
         self.top_temp = top_temp
         self.thermocline = thermocline
         self.energy = self.get_energy()
-        self.pathcost = 0 if time_slice==HORIZON else int(1e9)
+        self.pathcost = 0 if time_slice==HORIZON_HOURS else int(1e9)
         self.next_node = None
 
     def __repr__(self):
         return f"Node[time_slice:{self.time_slice}, top_temp:{self.top_temp}, thermocline:{self.thermocline}]"
 
     def get_energy(self):
-        m_layer = MASS_TANKS / NUM_LAYERS
+        m_layer = MASS_TANKS_KG / NUM_LAYERS
         energy_above = self.thermocline*m_layer * SPECIFIC_HEAT_WATER * to_celcius(self.top_temp)
-        energy_below = (NUM_LAYERS-self.thermocline)*m_layer * SPECIFIC_HEAT_WATER * to_celcius(self.top_temp - TEMP_LIFT)
+        energy_below = (NUM_LAYERS-self.thermocline)*m_layer * SPECIFIC_HEAT_WATER * to_celcius(self.top_temp - TEMP_LIFT_F)
         return energy_above + energy_below
 
 
@@ -61,7 +61,7 @@ class Graph():
         self.get_forecasts()
         self.create_nodes()
         self.create_edges()
-        # for h in range(HORIZON+1):
+        # for h in range(HORIZON_HOURS+1):
         #     print(f"Hour {h} has {len(self.nodes[h])} nodes")
         #     for n in self.nodes[h]:
         #         print(f"-{n}")
@@ -71,7 +71,7 @@ class Graph():
         print(f"Done in {round(time.time()-timer,1)} seconds.\n")
 
     def get_forecasts(self):
-        df = get_data(self.start_time, HORIZON)
+        df = get_data(self.start_time, HORIZON_HOURS)
         self.elec_prices = list(df.elec_prices)
         self.elec_prices = list(df.jan24_prices)
         # self.elec_prices = list(df.jul24_prices)
@@ -81,17 +81,17 @@ class Graph():
 
     def create_nodes(self):
         self.nodes = {}
-        for time_slice in range(HORIZON+1):
+        for time_slice in range(HORIZON_HOURS+1):
             self.nodes[time_slice] = [self.source_node] if time_slice==0 else []
             self.nodes[time_slice].extend(
                 Node(time_slice, top_temp, thermocline)
-                for top_temp in range(MIN_TOP_TEMP, MAX_TOP_TEMP+TEMP_LIFT, TEMP_LIFT) 
+                for top_temp in range(MIN_TOP_TEMP_F, MAX_TOP_TEMP_F+TEMP_LIFT_F, TEMP_LIFT_F) 
                 for thermocline in [1] + list(range(100, NUM_LAYERS + 1, 100))
                 if (time_slice, top_temp, thermocline) != (0, self.source_node.top_temp, self.source_node.thermocline)
             )
         
         # Add the min and max nodes in the first time slice
-        for h in range(HORIZON+1):
+        for h in range(HORIZON_HOURS+1):
             for node in self.nodes[h]:
                 # Discharging: find node that minimizes heat_output_HP
                 if h<ADD_MIN_HOURS:
@@ -99,9 +99,9 @@ class Graph():
                     toptemp = node.top_temp
                     while True:
                         if thermoc==1:
-                            if toptemp-TEMP_DROP < MIN_TOP_TEMP:
+                            if toptemp-TEMP_DROP_F < MIN_TOP_TEMP_F:
                                 break
-                            toptemp = toptemp-TEMP_DROP
+                            toptemp = toptemp-TEMP_DROP_F
                             thermoc = NUM_LAYERS
                         node_next = Node(h+1, thermocline=thermoc, top_temp=toptemp)
                         energy_to_store = node_next.energy - node.energy
@@ -119,13 +119,13 @@ class Graph():
                     toptemp = node.top_temp
                     while True:
                         if thermoc==NUM_LAYERS:
-                            if toptemp+TEMP_LIFT > MAX_TOP_TEMP:
+                            if toptemp+TEMP_LIFT_F > MAX_TOP_TEMP_F:
                                 break
-                            toptemp = toptemp+TEMP_LIFT
+                            toptemp = toptemp+TEMP_LIFT_F
                             thermoc = 1
                         node_next = Node(h+1, thermocline=thermoc, top_temp=toptemp)
                         energy_to_store = node_next.energy - node.energy
-                        if  energy_to_store + self.load[h] > HP_POWER:
+                        if  energy_to_store + self.load[h] > HP_POWER_KW:
                             thermoc += -1
                             if thermoc == 0:
                                 thermoc = NUM_LAYERS-1
@@ -136,15 +136,15 @@ class Graph():
     
     def create_edges(self):
         self.edges = {}
-        min_energy = Node(0,MIN_TOP_TEMP,1).energy
-        for h in range(HORIZON):
+        min_energy = Node(0,MIN_TOP_TEMP_F,1).energy
+        for h in range(HORIZON_HOURS):
             for node_now in self.nodes[h]:
                 self.edges[node_now] = []
                 for node_next in self.nodes[h+1]:
                     heat_to_store = node_next.energy - node_now.energy
                     losses = 0.005*(node_now.energy-min_energy)
                     heat_output_HP = heat_to_store + self.load[h] + losses
-                    if heat_output_HP <= HP_POWER and heat_output_HP>-0.5:
+                    if heat_output_HP <= HP_POWER_KW and heat_output_HP>-0.5:
                         cop = COP(oat=self.oat[h], lwt=to_celcius(node_next.top_temp))
                         cost = self.elec_prices[h]/100 * heat_output_HP / cop
                         if PUNISH_LOW_HP and h==0 and heat_output_HP < 2 and heat_output_HP > 0.05:
@@ -152,12 +152,12 @@ class Graph():
                         if heat_to_store > 0:
                             if node_next.top_temp==node_now.top_temp and node_next.thermocline>node_now.thermocline:
                                 self.edges[node_now].append(Edge(node_now, node_next, cost, heat_output_HP))
-                            if node_next.top_temp==node_now.top_temp+TEMP_LIFT:
+                            if node_next.top_temp==node_now.top_temp+TEMP_LIFT_F:
                                 self.edges[node_now].append(Edge(node_now, node_next, cost, heat_output_HP))
                         elif heat_to_store < 0:
                             if node_next.top_temp==node_now.top_temp and node_next.thermocline<node_now.thermocline:
                                 self.edges[node_now].append(Edge(node_now, node_next, cost, heat_output_HP))
-                            if node_next.top_temp==node_now.top_temp-TEMP_DROP:
+                            if node_next.top_temp==node_now.top_temp-TEMP_DROP_F:
                                 self.edges[node_now].append(Edge(node_now, node_next, cost, heat_output_HP))
                         else:
                             self.edges[node_now].append(Edge(node_now, node_next, cost, heat_output_HP))
@@ -165,7 +165,7 @@ class Graph():
     def solve_dijkstra(self):
         print("Solving Dijkstra...")
         start_time = time.time()
-        for time_slice in range(HORIZON-1, -1, -1):
+        for time_slice in range(HORIZON_HOURS-1, -1, -1):
             for node in self.nodes[time_slice]:
                 best_edge = min(self.edges[node], key=lambda e: e.head.pathcost + e.cost)
                 if best_edge.heat_output_HP<0: 
@@ -184,7 +184,7 @@ class Graph():
         max_edge_elec_input_HP = max_edge.heat_output_HP / cop_max_edge
         min_edge_elec_input_HP = min_edge.heat_output_HP / cop_min_edge
         bid = (min_edge.head.pathcost - max_edge.head.pathcost) / (max_edge_elec_input_HP - min_edge_elec_input_HP)
-        if min_edge.head.top_temp==MIN_TOP_TEMP and min_edge.head.thermocline==1 and min_edge.heat_output_HP>1:
+        if min_edge.head.top_temp==MIN_TOP_TEMP_F and min_edge.head.thermocline==1 and min_edge.heat_output_HP>1:
             print(f"Warning: The house will go cold if we don't buy now (will not receive missing {round(min_edge.heat_output_HP,1)} kWh)")
         self.bid = bid
         print(f"Buy electricity if it costs less than {round(bid*100,2)} cts/kWh\n")
@@ -206,13 +206,13 @@ class Graph():
         self.list_thermoclines.append(node_i.thermocline)
         self.list_hp_energy.append(edge_i.heat_output_HP)
         self.list_storage_energy.append(node_i.energy)
-        min_energy = Node(0,MIN_TOP_TEMP,1).energy
-        max_energy = Node(0,MAX_TOP_TEMP,NUM_LAYERS).energy
+        min_energy = Node(0,MIN_TOP_TEMP_F,1).energy
+        max_energy = Node(0,MAX_TOP_TEMP_F,NUM_LAYERS).energy
         list_soc = [(x-min_energy)/(max_energy-min_energy)*100 for x in self.list_storage_energy]
         # Plot the shortest path
         fig, ax = plt.subplots(2,1, sharex=True, figsize=(10,6))
         begin = self.start_time.format('YYYY-MM-DD HH:mm')
-        end = self.start_time.add(hours=HORIZON).format('YYYY-MM-DD HH:mm')
+        end = self.start_time.add(hours=HORIZON_HOURS).format('YYYY-MM-DD HH:mm')
         fig.suptitle(f'From {begin} to {end}\nCost: {round(self.source_node.pathcost,2)} $', fontsize=10)
         list_time = list(range(len(list_soc)))
         # Top plot
@@ -227,10 +227,10 @@ class Graph():
         ax2.set_ylabel('Electricity price [cts/kWh]')
         if min(self.elec_prices)>0: ax2.set_ylim([0,60])
         # Bottom plot
-        norm = Normalize(vmin=MIN_TOP_TEMP-TEMP_LIFT, vmax=MAX_TOP_TEMP)
+        norm = Normalize(vmin=MIN_TOP_TEMP_F-TEMP_LIFT_F, vmax=MAX_TOP_TEMP_F)
         cmap = matplotlib.colormaps['Reds']
         tank_top_colors = [cmap(norm(x)) for x in self.list_toptemps]
-        tank_bottom_colors = [cmap(norm(x-TEMP_LIFT)) for x in self.list_toptemps]
+        tank_bottom_colors = [cmap(norm(x-TEMP_LIFT_F)) for x in self.list_toptemps]
         list_thermoclines_reversed = [NUM_LAYERS-x+1 for x in self.list_thermoclines]
         ax[1].bar(list_time, list_thermoclines_reversed, color=tank_bottom_colors, alpha=0.7)
         ax[1].bar(list_time, self.list_thermoclines, bottom=list_thermoclines_reversed, color=tank_top_colors, alpha=0.7)
