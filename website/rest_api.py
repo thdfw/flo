4046cf8e01c2925f54c63f2cd0722a315d646b9b
thdfw <th.defauw@gmail.com>
2024-10-07 12:19:37 -0700

    Debug

--- website/rest_api.py
+++ website/rest_api.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import dotenv
 import pendulum
@@ -13,6 +14,15 @@ engine = create_engine(settings.db_url.get_secret_value())
 Session = sessionmaker(bind=engine)
 
 app = FastAPI()
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class ThermostatRequest(BaseModel):
     password: str
