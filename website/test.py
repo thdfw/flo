66b976cc2d735bd773363273639917846e751c63
thdfw <th.defauw@gmail.com>
2024-10-07 10:21:53 -0700

    working api with temperatures

---
+++ website/test.py
@@ -0,0 +1,47 @@
+import dotenv
+import pendulum
+from fastapi import FastAPI, HTTPException
+from sqlalchemy import create_engine, desc
+from sqlalchemy.orm import sessionmaker
+from gjk.config import Settings
+from gjk.models import MessageSql
+
+settings = Settings(_env_file=dotenv.find_dotenv())
+engine = create_engine(settings.db_url.get_secret_value())
+Session = sessionmaker(bind=engine)
+
+app = FastAPI()
+
+@app.get("/thermostats/{house_alias}")
+async def get_latest_temperature(house_alias: str):
+    
+    session = Session()
+
+    timezone = "America/New_York"
+    start = pendulum.datetime(2022, 1, 1, 0, 0, tz=timezone)
+    start_ms = int(start.timestamp() * 1000)
+
+    last_message = session.query(MessageSql).filter(
+        MessageSql.from_alias.like(f'%{house_alias}%'),
+        MessageSql.message_persisted_ms >= start_ms
+    ).order_by(desc(MessageSql.message_persisted_ms)).first()
+
+    if not last_message:
+        raise HTTPException(status_code=404, detail="No messages found.")
+                    
+    temperature_data = []
+    for elem in last_message.payload['DataChannelList']:
+        if 'zone' in elem['Name'] and 'gw' not in elem['Name']:
+            zone_name = elem['Name']
+            for reading in last_message.payload['ChannelReadingList']:
+                if reading['ChannelId'] == elem['Id']:
+                    temperature_data.append({
+                        "zone": zone_name,
+                        "temperature": reading['ValueList'][0] / 1000
+                    })
+
+    return temperature_data
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
